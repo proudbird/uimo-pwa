c6ed327e6db1e5907591e9c912b8c6f6037915d4
proudbird <v6825403@hotmail.com>
2024-01-03 18:58:04 +0100

    revision VariableDataAttribute

--- platform/core/data/attribute/variable.ts
+++ platform/core/data/attribute/variable.ts
@@ -3,14 +3,21 @@ import { IView } from '@/core/types';
 
 import { DataAttribute } from '../state';
 import DataAttributeBase from './dataAttribute';
+import { DataAttributeChangeEvent } from '../events';
+
+export interface VariableAttributeOptions {
+	initValue?: any;
+	inScope?: boolean;
+}
 
 export default class VariableAttribute extends DataAttributeBase {
 	#value: any;
+	#prevValue: any = undefined;
 
-	constructor(value: any, owner: IView, parent?: DataAttribute) {
-		super(owner, parent);
+	constructor({ initValue, inScope }: VariableAttributeOptions, owner?: IView, parent?: DataAttribute) {
+		super(owner, parent, inScope);
 
-		this.#value = value;
+		this.#value = initValue;
 	}
   
 	get value(): null {
@@ -18,6 +25,9 @@ export default class VariableAttribute extends DataAttributeBase {
 	}
 
 	set value(value: any) {
+		this.#prevValue = this.#value;
 		this.#value = value;
+
+		this.dispatchEvent(new DataAttributeChangeEvent(this, this.#value, this.#prevValue));
 	}
 }
