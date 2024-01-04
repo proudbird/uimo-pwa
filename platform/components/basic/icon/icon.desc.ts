47128d85e3058538c3d7ccae63695ba3215d5dcb
proudbird <v6825403@hotmail.com>
2024-01-04 09:37:33 +0100

    feat: icons from different fonts

--- platform/components/basic/icon/icon.desc.ts
+++ platform/components/basic/icon/icon.desc.ts
@@ -1,13 +1,7 @@
 export default {
   props: {
     variant: {
-      type: [
-        { value: 'home', label: 'Home' },
-        { value: 'dashboard', label: 'Dashboard' },
-        { value: 'inventory_2', label: 'Inventory 2' },
-        { value: 'close', label: 'Close' },
-        { value: 'more-horiz', label: 'More Horiz' },
-      ]
+      type: 'string'
     },
     size: {
       type: 'string',
--- platform/components/basic/icon/icon.ts
+++ platform/components/basic/icon/icon.ts
@@ -1,14 +1,49 @@
 import { Component, DefineComponent } from '@/core';
+import { getProperty } from '@/core/fabric';
 
 import { specification, IIconComponent } from './icon.types';
+import { PropHandlerDefinition } from '@/core/types';
 
 @DefineComponent('icon')
 export default class Icon extends Component<IIconComponent>(specification) {
   render() {
     return {
       ...this.config,
+      className: {
+        handler: () => {
+          const { set, variant } = getIconDefinition(this.props.variant);
+          let passedClassName = getProperty(this.config.className) || '';
+          let className = '';
+
+          switch (true) {
+            case set === 'mso':
+              className = 'mso';
+              break;
+            case set === 'vsc':
+              className = `codicon codicon-${variant}`;
+              break;
+            case set === 'urs':
+              className = `urs fi-rs-${variant}`;
+              break;
+            default:
+              break;
+          }
+
+          return `${passedClassName} ${className}`
+        },
+        dependencies: (this.config?.className as PropHandlerDefinition)?.dependencies
+      },
       props: {
-        innerHTML: this.props.variant
+        innerHTML: {
+          handler: () => {
+            const { set, variant } = getIconDefinition(this.props.variant);
+            if(set === 'mso') {
+              return variant;
+            } else {
+              return '';
+            }
+          }
+        }
       },
       style: {
         fontSize: this.props.size
@@ -17,3 +52,18 @@ export default class Icon extends Component<IIconComponent>(specification) {
   }
 }
 
+function getIconDefinition(icon: string) {
+  let set = 'mso';
+  let variant = '';
+
+  const parts = icon.split(':');
+
+  if(parts.length === 2) {
+    set = parts[0];
+    variant = parts[1];
+  } else {
+    variant = icon;
+  }
+
+  return { set, variant };
+} 
